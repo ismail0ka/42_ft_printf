dd9aa3881ec1a3f4fcba325d38e2a4cdc1bf5c39
Ismail Karouata <ikarouat@c4r10p9.1337.ma>
2024-12-11 03:44:08 +0100

    new impl

--- ft_printf_utils/ft_printf_utils.h
+++ ft_printf_utils/ft_printf_utils.h
@@ -5,37 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikarouat <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/06 04:37:47 by ikarouat          #+#    #+#             */
-/*   Updated: 2024/12/08 00:16:44 by ikarouat         ###   ########.fr       */
+/*   Created: 2024/12/10 20:35:51 by ikarouat          #+#    #+#             */
+/*   Updated: 2024/12/11 03:37:17 by ikarouat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_UTILS_H
 # define FT_PRINTF_UTILS_H
+
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdarg.h>
-# include <stdint.h>
-# ifndef FLAGS
-#  define FLAGS "-0.# +"
-# endif
-# ifndef FORMATS
-#  define FORMATS "cspdiuxX"
-# endif
 
-typedef unsigned int	t_uint;
-typedef union u_frmt	t_frmt;
-union	u_frmt
+typedef struct s_frmt_ctx
 {
-	uintptr_t	p;
-	t_uint		u;
-	char		c;
-	char		*s;
-	int			xdi;
-};
-int		check_format(const char *f, va_list *ap);
-int		check_flags(const char *f);
-char	*ft_strchr(const char *s, int c);
-t_frmt	ft_putnbr_base(t_frmt n, unsigned int base);
-t_frmt	ft_putstr(char *s);
-#endif
+	char	has_zero_flag;//if minus flag is set unset zero
+	char	has_minus_flag;
+	char	has_hash_flag;
+	char	has_space_flag;
+	char	has_plus_flag;
+	size_t	precision;
+	char	*buffer;
+	size_t	buffer_len;
+} t_frmt_ctx;
+
+
+
+#endif //FT_PRINTF_UTILS_H
