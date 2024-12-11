3936d8d9ef2aa87e2d35b5f4cae6532c4f5be911
Ismail Karouata <ikarouat@c4r10p9.1337.ma>
2024-12-11 03:50:46 +0100

    0x0

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ikarouat <ikarouat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 15:45:20 by ikarouat          #+#    #+#             */
-/*   Updated: 2024/12/08 00:15:30 by ikarouat         ###   ########.fr       */
+/*   Updated: 2024/12/11 03:50:19 by ikarouat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,27 @@
 
 int	printf(const char *s, ...)
 {
-	va_list	ap;
+	va_list		ap;
+	int		len;
+	t_frmt_ctx	frmt_ctx;
 
+	len = 0;
 	va_start(ap, s);
-	while (*(s + 1) && *s)
+	init_format_context(&frmt_ctx);
+	while (*s)
 	{
+		len++;
 		if (*s++ == '%')
-			check_format(s, &ap);
+		{
+			//check for flags
+			//check format specifier
+		}
 	}
 	va_end(ap);
 	return ;
 }
+
+
 //flag 0n applies to n in (diuxX) pad left with n zeros
 //flag -n (overrides flag 0) pad right with blanks
 //flag .n shows n bytes from value
